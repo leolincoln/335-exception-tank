049e903296824cb0d527f25c462d87c5e65dc96b
leoliu@email.arizona.edu <leoliu@email.arizona.edu@808fc368-2ffd-c855-467e-e157569092d2>
2011-11-28 22:56:00 +0000

    full screen on GUI

--- trunk/Tank/src/GUI.java
+++ trunk/Tank/src/GUI.java
@@ -1,13 +1,16 @@
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
 
 public class GUI {
+	private static Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 	
 	public static void main(String[] args) {
 		TankView t = new TankView();
-		t.setSize(1000, 1000);
+		t.setSize(screen);
 		t.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		t.setBackground(Color.BLACK);
 		t.setVisible(true);
